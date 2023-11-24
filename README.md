b80cdb3f9d7c4e5e3a5f6e000551a06933593054
Khaelano Abroor Maulana <khaelanoabroor@gmail.com>
2023-11-24 15:18:01 +0700

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+## Donat
+This is an implementation of the infamous spinning donut code in Rust.
+Not really similar, but it works.
+
--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,40 @@ use std::f32::consts::PI;
 use std::{thread::sleep, time::Duration};
 
 mod utils;
-use utils::{torus, rotate_y, rotate_x, rotate_z};
+use utils::{rotate_x, rotate_y, rotate_z, torus};
 
-const S_WIDTH: usize = 30_usize;
-const S_HEIGHT: usize = 30_usize;
+const S_WIDTH: usize = 50_usize;
+const S_HEIGHT: usize = 50_usize;
 
 const THETA_INCR: f32 = 0.07;
 const PHI_INCR: f32 = 0.02;
-const R1: f32 = 1.00;   // radius of circle that revolves an axis to make torus
-const R2: f32 = 2.00;   // radius of torus
+const R1: f32 = 1.00; // radius of circle that revolves an axis to make torus
+const R2: f32 = 2.00; // radius of torus
 
-const K2: f32 = 5.00;   // distance from viewer
-const K1: f32 = 15.0;   // FOV
-//const K1: f32 = (S_WIDTH as f32 * K2 * 3.0) / (8.0 * (R1 + R2));   // FOV
+const K2: f32 = 8.00; // distance from viewer
+// const K1: f32 = 15.0; // FOV
+const K1: f32 = (S_WIDTH as f32 * K2 * 3.0) / (8.0 * (R1 + R2));   // FOV
 
-fn main () -> ! {
+fn main() -> ! {
     print!("\x1b[2J");
     // represents the current angle of torus
     let (mut a, mut b) = (0.0, 90.0);
 
     loop {
-        a += 0.4; 
+        a += 0.2;
         b += 0.2;
 
         // reset either a or b above if its value above 2 * PI
-        if a > 2.0 * PI {a = 0.0;}
-        if b > 2.0 * PI {b = 0.0;}
+        if a > 2.0 * PI {
+            a = 0.0;
+        }
+        if b > 2.0 * PI {
+            b = 0.0;
+        }
 
         // create output and zbuffer
-        let mut output = [[' ';S_WIDTH];S_HEIGHT];
-        let mut zbuffer = [[0.0f32;S_WIDTH];S_HEIGHT];
+        let mut output = [[' '; S_WIDTH]; S_HEIGHT];
+        let mut zbuffer = [[0.0f32; S_WIDTH]; S_HEIGHT];
 
         // the loop of circle
         let mut theta = 0.0;
@@ -49,9 +53,9 @@ fn main () -> ! {
                 (tx, ty, tz) = rotate_x(tx, ty, tz, a);
                 //(tx, ty, tz) = rotate_y(tx, ty, tz, a);
                 (tx, ty, tz) = rotate_z(tx, ty, tz, b);
-                
+
                 tz += K2; // added with K2 because K2 is the distance of point from screen
-                let ooz = 1.0/tz;
+                let ooz = 1.0 / tz;
 
                 // create 2d projection of torus
                 let p_tx = S_WIDTH as i32 / 2 + (K1 * tx * ooz) as i32;
@@ -70,10 +74,11 @@ fn main () -> ! {
 
                 if l > 0.0 && ooz > zbuffer[p_ty as usize][p_tx as usize] {
                     zbuffer[p_ty as usize][p_tx as usize] = ooz;
+                    // output[p_ty as usize][p_tx as usize] = "@$#*!=;:^~,."
                     output[p_ty as usize][p_tx as usize] = ".,-~:;=!*#$@"
-                                                            .chars()
-                                                            .nth(l_index)
-                                                            .expect("Error cannot indexp_ty as usize luminance level");
+                        .chars()
+                        .nth(l_index)
+                        .expect("Error cannot indexp_ty as usize luminance level");
                 }
             }
         }
@@ -86,6 +91,6 @@ fn main () -> ! {
             println!()
         }
 
-        sleep(Duration::from_millis(50));
+        sleep(Duration::from_millis(500));
     }
 }
